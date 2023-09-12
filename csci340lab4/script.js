9abd5f26bc40e46d9ca989bad1ff0c6d33ae4ba2
Htjackson8506 <104510246+Htjackson8506@users.noreply.github.com>
2023-09-12 16:50:44 -0700

    changed cat API to the one from shibe.online for cors compatibility and added .ajax

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -6,15 +6,21 @@ $(document).ready(function() {
             dataType: 'json',
             success: function(results) {
                 console.log(results["message"]);
-                if (results["message"].endsWith(".mp4")) {
-                    $('#dogImage').attr("src", "images/blank.png");
-                } else {
-                    $('#dogImage').attr("src", results["message"]);
-                }
+                $('#dogImage').attr("src", results["message"])
             },
             error: function(xhr,status,error) {
                 console.log(error);
             }
     });
 });
+    $('#catClicker').click(function(){
+        $.ajax({
+            url: "https://shibe.online/api/cats",
+            dataType: 'json',
+            success: function(results) {
+                console.log(results[0]);
+                $('#catImage').attr("src", results[0])
+            }
+        })
+    })
 });

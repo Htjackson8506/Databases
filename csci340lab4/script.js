b57aac1b5e0ed8cf48d477a2fbcf2d921c1f7e18
Htjackson8506 <104510246+Htjackson8506@users.noreply.github.com>
2023-09-12 16:41:17 -0700

    changed "url" tag to match new API

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
             url: 'https://dog.ceo/api/breeds/image/random',
             dataType: 'json',
             success: function(results) {
-                console.log(results["url"]);
-                if (results["url"].endsWith(".mp4")) {
+                console.log(results["message"]);
+                if (results["message"].endsWith(".mp4")) {
                     $('#duckImage').attr("src", "images/blank.png");
                 } else {
-                    $('#duckImage').attr("src", results["url"]);
+                    $('#duckImage').attr("src", results["message"]);
                 }
             },
             error: function(xhr,status,error) {

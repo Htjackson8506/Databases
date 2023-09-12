e163f17b17250990acf0a284532a6b2c23294830
Htjackson8506 <104510246+Htjackson8506@users.noreply.github.com>
2023-09-12 14:06:14 -0700

    duck api troubleshooting

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -2,7 +2,7 @@ let isDuck = true;
 
 function fetchDuckImage() {
     $.ajax({
-        url: 'https://random-d.uk/api',
+        url: 'https://random-d.uk/api/v2',
         type: 'GET',
         dataType: 'json',
         success: function(data) {

bfd7329423255f8e3072759de042780d841e5d46
Htjackson8506 <104510246+Htjackson8506@users.noreply.github.com>
2023-09-12 16:05:19 -0700

    removed duck .ajax to test cat .ajax

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -2,19 +2,19 @@
 $(document).ready(function() {
     $('#clicker').click(function() {
         $.ajax({
-            url: 'https://random-d.uk/api/quack',
+            url: "https://api.thecatapi.com/v1/images/search",
             dataType: 'json',
-            success: function(results) {
+            success: function(data) {
                 console.log(results["url"]);
                 if (results["url"].endsWith(".mp4")) {
-                    $('#duckImage').attr("src", "images/blank.png");
+                    $('#catImage').attr("src", "images/blank.png");
                 } else {
-                    $('#duckImage').attr("src", results["url"]);
+                    $('#catImage').attr("src", results["url"]);
                 }
             },
             error: function(error) {
-                console.error("Error fetching duck image:", error);
-            }
+                console.error("Error fetching cat image:", error);
+            }   
+        });
     });
-});
 });

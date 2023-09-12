913f7b3658a1bc657533f111d5d5194f82c7e20d
Htjackson8506 <104510246+Htjackson8506@users.noreply.github.com>
2023-09-12 14:22:38 -0700

    script fix

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,24 +1,21 @@
-let isDuck = true;
 
-function fetchDuckImage() {
-    $.ajax({
-        url: 'https://random-d.uk/',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            document.getElementById('duckImage').src = data.url;
-        },
-        error: function(error) {
-            console.error("Error fetching duck image:", error);
-        }
-    });
+$(document).ready(function() {
+    $('#clicker').click(function() {
+    function fetchDuckImage() {
+        $.ajax({
+            url: 'https://random-d.uk/',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                document.getElementById('duckImage').src = data.url;
+            },
+            error: function(error) {
+                console.error("Error fetching duck image:", error);
+            }
+        });
 }
 
 function fetchCatImage() {
     document.getElementById('catImage').src = 'https://api.thecatapi.com/v1/images/search';
 }
-
-$(document).ready(function() {
-    fetchDuckImage();
-    fetchCatImage();
-});
+    })})
